47664604d5645398cfdb3ea24cddc02b40f98f06
Aashish Welling <omegablitz@users.noreply.github.com>
2022-04-13 00:31:07 -0400

    format

--- src/Dagger/View.hs
+++ src/Dagger/View.hs
@@ -20,15 +20,14 @@ nodeView = \case
     Float d -> div_ [class_ "bg-red-400 grid grid-cols-1 m-1 p-1"] [text . ms $ show d]
     String t -> div_ [class_ "bg-red-400 grid grid-cols-1 m-1 p-1"] [text $ ms t]
     Bytes b -> div_ [class_ "bg-red-400 grid grid-cols-1 m-1 p-1"] [text . ms $ show b]
-    List ns -> div_ [class_ "bg-blue-400 bg-opacity-50"] $ zip [0 :: Int ..] (toList ns) <&> \(i, n) -> div_ [class_ "grid grid-cols-2"]
-        [ div_ [class_ "bg-green-400 m-1 p-1"] [text . ms $ show i]
-        , div_ [class_ ""] [nodeView n]
-        ]
-    Map m -> div_ [class_ "bg-indigo-400 bg-opacity-50"] $ M.toList m <&> \(k, n) -> div_ [class_ "grid grid-cols-2"]
-        [ div_ [class_ "bg-yellow-400 grid grid-cols-1 m-1 p-1"] [text $ ms k]
-        , div_ [class_ ""] [nodeView n]
-        ]
-    Link b -> div_ [style_ $ nodeStyle "#DDFFFF"] [text . ms $ show b]
-
-nodeStyle :: MisoString -> Map MisoString MisoString
-nodeStyle _ = []
+    List ns -> div_ [class_ "bg-blue-400 bg-opacity-50"] $ zip [0 :: Int ..] (toList ns) <&> \(i, n) ->
+        div_ [class_ "grid grid-cols-2"]
+            [ div_ [class_ "bg-green-400 m-1 p-1"] [text . ms $ show i]
+            , div_ [] [nodeView n]
+            ]
+    Map m -> div_ [class_ "bg-indigo-400 bg-opacity-50"] $ M.toList m <&> \(k, n) ->
+        div_ [class_ "grid grid-cols-2"]
+            [ div_ [class_ "bg-yellow-400 grid grid-cols-1 m-1 p-1"] [text $ ms k]
+            , div_ [] [nodeView n]
+            ]
+    Link b -> div_ [] [text . ms $ show b]
